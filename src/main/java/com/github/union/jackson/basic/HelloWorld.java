651f93006711bee71b2b8beb962ce486533eb9ce
UnionOne <hax2033@gmail.com>
2016-04-26 10:29:00 +0300

    Hello World commit

---
+++ src/main/java/com/github/union/jackson/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.github.union.jackson.basic;
+
+/**
+ * Created by union on 26.04.16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
